330da088c326c900fb6c77e002dc262dbfd2b056
Higor de Faria <higorpr@gmail.com>
2022-08-16 01:25:30 -0300

    Requirement implementation
    Implementation of Card Distribution requirement 2:
    - User is asked number of cards until an even number is inputted (function cardsNumber)

--- java/script.js
+++ java/script.js
@@ -4,7 +4,7 @@ let n_cards;
 // Functions
 function cardsNumber() {
     n_cards = prompt('Memory Game - You would like to play with how many cards (4-14 cards)?')
-    if (n_cards % 2 !== 0) {
-        n_cards = prompt('You cannot pick an odd number of cards in a memory game. Please ')
+    while (n_cards % 2 !==0) {
+        n_cards = prompt('You cannot pick an odd number of cards in a memory game. Please pick an even number between between 4 and 14:')
     }
 }
