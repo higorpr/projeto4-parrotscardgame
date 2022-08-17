7ac7225e5c96a64cde20140297c0e7b2b0b33967
Higor de Faria <higorpr@gmail.com>
2022-08-16 23:11:07 -0300

    Update of cardsNumber function
    Updating function to comply with card number rules defined in the project's requirements.

--- java/script.js
+++ java/script.js
@@ -4,7 +4,12 @@ let n_cards;
 // Functions
 function cardsNumber() {
     n_cards = prompt('Memory Game - You would like to play with how many cards (4-14 cards)?')
-    while (n_cards % 2 !==0) {
-        n_cards = prompt('You cannot pick an odd number of cards in a memory game. Please pick an even number between between 4 and 14:')
+    while (n_cards % 2 !==0 || n_cards === undefined || n_cards < 4 || n_cards > 14) {
+        n_cards = prompt(
+        `Rules for number of cards:
+        - There must be an even number of cards;
+        - The number of cards in a game must be between 4 and 14.
+        
+        Please pick a number of cards within these conditions:`)
     }
 }
