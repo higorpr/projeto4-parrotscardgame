c7001a2ebfc91bf1422c6d6240eb97c0f397f882
Higor de Faria <higorpr@gmail.com>
2022-08-19 01:07:16 -0300

    Card Rotation (Raw)

--- java/script.js
+++ java/script.js
@@ -3,7 +3,9 @@ let n_cards;
 
 // Functions
 function cardsNumber() {
-    n_cards = prompt('Memory Game - You would like to play with how many cards (4-14 cards)?')
+
+    n_cards = prompt('Memory Game - You would like to play with how many cards (4-14 cards)?');
+
     while (n_cards % 2 !==0 || n_cards === undefined || n_cards < 4 || n_cards > 14) {
         n_cards = prompt(
         `Rules for number of cards:
@@ -13,16 +15,28 @@ function cardsNumber() {
         Please pick a number of cards within these conditions:`);
     }
 
-    let list = document.querySelector('.card_holder');
+    let list1 = document.querySelector('.row1');
+    let list2 = document.querySelector('.row2');
 
-    for (i=0; i < n_cards; i++) {
-        list.innerHTML = list.innerHTML + 
-        `<li class="card">
-            <figure>
-                <img src="images/front.png" alt="">
-            </figure>
-        </li>`
+    let card = 
+    `<li onclick="rotateCard(this)" class="card">
+        <figure>
+            <img src="images/front.png" alt="">
+        </figure>
+    </li>`
+
+    for (i=0; i < n_cards/2; i++) {
+        list1.innerHTML = list1.innerHTML + card;
     }
 
+    for (i=0; i < n_cards/2; i++) {
+        list2.innerHTML = list2.innerHTML + card;
+    }
+}
 
+cardsNumber()
+
+function rotateCard(card) {
+    
+    card.classList.toggle('card_transition');
 }
